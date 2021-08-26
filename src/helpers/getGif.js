e5df8b1a31c6a6a5ea69f03cb386f91f288efbf5
Alex Rodriguez Huerta <arodriguez2407@gmail.com>
2021-08-26 18:37:32 -0500

    https actualizado

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,5 +1,5 @@
 const getGif = async(category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=awOFujaD938lcJvQnMm3kqDs40wdPoqd`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=awOFujaD938lcJvQnMm3kqDs40wdPoqd`
     const resp = await fetch(url)
     const {data} = await resp.json()
     
